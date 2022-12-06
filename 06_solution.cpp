0000014010416ee410f2f24ac6f0bfefeba9a60d
Fee <freakyfee@proton.me>
2022-12-06 18:45:12 +0100

    goto solution

--- 06_solution.cpp
+++ 06_solution.cpp
@@ -3,20 +3,17 @@
 
 template<size_t L=4>
 int find_start_marker(std::string packet){
-    std::string stuffs(L,0);
-    for(int i=0;i<packet.size();i++){
+    std::string stuffs = packet.substr(0,L);
+    for(int i=L;i<packet.size();i++){
         stuffs[i%L] = packet[i];
-        if(stuffs.find('\0') != -1)
-            continue; //buffer is not full yet
-        bool start = true;
         for(int j=0; j<L; j++){
             for(int k=0; k<j; k++){
                 if (stuffs[k]==stuffs[j])
-                    start = false; //duplicate in buffer
+                    goto duplicate;
             }
         }
-        if(start)
-            return i;
+        return i;
+        duplicate:;
     }
     return -1;
 }
